dd6a2a56893bf290ae7b4aa8e77c8bd3d7ba4328
user <example@gmail.com>
2023-10-14 21:55:35 +0000

    Repository commit

--- src/main/java/com/example/Project2/demo/project2/SolutionsRepository.java
+++ src/main/java/com/example/Project2/demo/project2/SolutionsRepository.java
@@ -2,6 +2,6 @@ package com.example.Project2.demo.project2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SolutionsRepository extends JpaRepository<Solutions, Long> {
+public interface SolutionsRepository extends JpaRepository<Solutions,Long> {
     Solutions findBySymptoms(String symptom);
 }
