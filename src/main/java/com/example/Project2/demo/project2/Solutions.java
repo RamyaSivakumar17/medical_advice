21e3dd3c6aadedb851ea92b8b08d323b3e3fcda1
user <example@gmail.com>
2023-10-14 21:54:51 +0000

    Model commit

---
+++ src/main/java/com/example/Project2/demo/project2/Solutions.java
@@ -0,0 +1,27 @@
+package com.example.Project2.demo.project2;
+
+public class Solutions {
+    private Long id;
+    private String symptom;
+    private String advice;
+
+    public Solutions() {
+    }
+
+    public String getSymptom() {
+        return symptom;
+    }
+
+    public void setSymptom(String symptom) {
+        this.symptom = symptom;
+    }
+
+    public String getAdvice() {
+        return advice;
+    }
+
+    public void setAdvice(String advice) {
+        this.advice = advice;
+    }
+}
+
---
+++ src/main/java/com/example/Project2/demo/project2/SolutionsRepository.java
@@ -0,0 +1,7 @@
+package com.example.Project2.demo.project2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SolutionsRepository extends JpaRepository<Solutions, Long> {
+    Solutions findBySymptoms(String symptom);
+}
